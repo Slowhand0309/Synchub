a84fd390a0a6d1c0481bdec6498f2baeb1506d7b
slowhand0309 <slowhand0309@gmail.com>
2015-11-08 16:33:13 +0900

    First commit

---
+++ lib/synchub.ex
@@ -0,0 +1,31 @@
+defmodule Synchub do
+  @moduledoc """
+  The module of sync github repos.
+  """
+  use HTTPotion.Base
+
+  @apiurl "https://api.github.com/"
+
+  @doc "handle create url."
+  def process_url(url) do
+    @apiurl <> url
+  end
+
+  @doc "handle create request header." 
+  def process_request_headers(headers) do
+    Dict.put headers, :"User-Agent", "Slowhand"
+  end
+
+  @doc "handle response of html body."
+  def process_response_body(body) do
+    body |> Poison.decode! |> put_repo_name
+  end
+
+  @doc "put repos name."
+  def put_repo_name([info|tail]) do
+    IO.puts info["name"]
+    put_repo_name(tail)
+  end
+
+  def put_repo_name([]), do: nil
+end
