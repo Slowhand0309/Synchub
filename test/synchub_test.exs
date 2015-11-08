d1a74ff517b3030d63c97b67dea70b92765d7cbd
slowhand0309 <slowhand0309@gmail.com>
2015-11-09 00:10:23 +0900

    Added test and travis.xml

--- test/synchub_test.exs
+++ test/synchub_test.exs
@@ -1,7 +1,20 @@
 defmodule SynchubTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "user-agent" do
+    headers = Synchub.process_request_headers(%{})
+    agent = Map.fetch!(headers, :"User-Agent")
+    assert agent == Application.get_env(:synchub, :username)
   end
+
+  test "list.github.repos" do
+url = "users/" <> Application.get_env(:synchub, :userid) <> "/repos"
+    Synchub.start
+    assert_response Synchub.get(url)
+  end
+
+  defp assert_response(response) do
+    assert HTTPotion.Response.success?(response, :extra)
+  end
+
 end
