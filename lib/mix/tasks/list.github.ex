ad95b4ef21b4e3998c32ce8a3e397fbbfb5eea7e
slowhand0309 <slowhand0309@gmail.com>
2015-12-07 01:10:44 +0900

    Fixed bug task for list repos

--- lib/mix/tasks/list.github.ex
+++ lib/mix/tasks/list.github.ex
@@ -7,6 +7,6 @@ defmodule Mix.Tasks.List.Github do
     url = "users/" <> Application.get_env(:synchub, :userid) <> "/repos"
     Synchub.start
     repos = Synchub.get(url)
-    Synchub.Listrepos.put_repo_name(repos)
+    Synchub.Listrepos.put_repo_name(repos.body)
   end
 end
